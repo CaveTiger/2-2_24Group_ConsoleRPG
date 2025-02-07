c146b53d4037cc7923fd17468a5921448af08a17
hyeondong00 <hydo1126@gmail.com>
2025-02-07 11:46:45 +0900

    SHOP&INVENTORY

---
+++ ConsoleRPG24/ConsoleRPG24/Shop.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleRPG24
+{
+    internal class Shop
+    {
+        void EnterShop(string location)
+        {
+            if (location == "마을")
+            {
+                Console.WriteLine("마을");
+                ShowVillageShop();
+            }
+            else if (location == "던전")
+            {
+                Console.WriteLine("?");
+                ShowDungeonShop();
+            }
+            else
+            {
+                Console.WriteLine("?");
+            }
+        }
+
+        void ShowVillageShop()
+        {
+            Console.WriteLine("[마을 상점]");
+            Console.WriteLine("1. ");
+            Console.WriteLine("2. ");
+            Console.WriteLine("0. ");
+        }
+
+        void ShowDungeonShop()
+        {
+            Console.WriteLine("[던전 상점]");
+            Console.WriteLine("1. ");
+            Console.WriteLine("2. ");
+            Console.WriteLine("0. ");
+        }
+    }
+}
