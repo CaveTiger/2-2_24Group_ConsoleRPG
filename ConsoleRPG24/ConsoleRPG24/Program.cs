060536fb9e3b50b2fdd580b88e03352be4a1e693
MTH_Inz <49317222+YujiHeo@users.noreply.github.com>
2025-02-07 10:10:50 +0900

    Revert "Yuji/Dev first commit"
    This reverts commit 5c33a40a2b87b29a38f712895cc0493c5ad40762.

--- ConsoleRPG24/ConsoleRPG24/Program.cs
+++ ConsoleRPG24/ConsoleRPG24/Program.cs
@@ -1,10 +1,10 @@
-﻿namespace Dungeon
+﻿namespace ConsoleRPG24
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, boy!!!!");
         }
     }
 }
