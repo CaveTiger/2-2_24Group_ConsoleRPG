3332155165c286dced0916e9ed855f3af1c919d3
whitechoco1029 <newtime1029@naver.com>
2025-02-07 16:30:24 +0900

    [ Delete ]용병 및 몬스터 마나 삭제
    mercenary,monster
    (-)int mana;

--- ConsoleRPG24/ConsoleRPG24/stat.cs
+++ ConsoleRPG24/ConsoleRPG24/stat.cs
@@ -102,7 +102,7 @@
 
             public class Mercenary : BaseCharacter
             {
-                public Mercenary(string name, int atk, int defn, float health, float maxHealth, int mana, int speed) : base(name, atk, defn, health, maxHealth, mana, speed)
+                public Mercenary(string name, int atk, int defn, float health, float maxHealth, int speed) : base(name, atk, defn, health, maxHealth, mana, speed)
                 {
 
                 }
@@ -117,7 +117,7 @@
             public class Monster : BaseCharacter
             {
                 public Monster(string name, int atk, int defen, float health, float maxHealth, int mana, int speed)
-                        : base(name, atk, defen, health, maxHealth, mana, speed)
+                        : base(name, atk, defen, health, maxHealth, speed)
                 {
 
                 }
