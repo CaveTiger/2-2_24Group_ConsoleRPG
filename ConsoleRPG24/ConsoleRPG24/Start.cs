1abaf5660ecfc68b40ac4ba6fb4bdc3db1c7d8fc
CaveTiger <kinusagi99@gmail.com>
2025-02-11 16:40:46 +0900

    재 작업 시작

---
+++ ConsoleRPG24/ConsoleRPG24/Start.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleRPG24
+{
+    internal class Start
+    {
+        static void Main(string[] args)
+        {
+            var battle = new BattleSystem();
+            battle.BattleStart();
+        }
+    }
+}
