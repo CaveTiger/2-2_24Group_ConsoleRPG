85999f64626d3f4cf817d1b50f037e09ec94ef87
MTH_Inz <49317222+YujiHeo@users.noreply.github.com>
2025-02-12 20:35:20 +0900

    Create StartGame.cs

---
+++ ConsoleRPG24/ConsoleRPG24/StartGame.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace ConsoleRPG24
+{   
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MainScreen game = new MainScreen();
+
+            game.GameStart();
+            //game.InventoryScreen();
+        }
+    }
+
+}
