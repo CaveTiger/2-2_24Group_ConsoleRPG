103bfa851434a67de714de474227c4440741b210
MTH_Inz <49317222+YujiHeo@users.noreply.github.com>
2025-02-12 20:47:40 +0900

    Update Dungeon.cs

--- ConsoleRPG24/ConsoleRPG24/Dungeon.cs
+++ ConsoleRPG24/ConsoleRPG24/Dungeon.cs
@@ -9,7 +9,7 @@ namespace ConsoleRPG24
 
     //던전 아직 구현 중입니다!!
 
-   internal class Stage
+    internal class Stage
     {
         Player player;
         List<Item> itemList = new List<Item>();
@@ -27,64 +27,72 @@ namespace ConsoleRPG24
         }
 
         //
-        public void Rewards(Player player) 
+        public void Rewards(Player player)
         {
             if (MainScreen.instance.player.IsDead)
 
                 Console.WriteLine($"스테이지 클리어! 적을 물리쳤다!");
-                Console.WriteLine("전리품으로써 골드 혹은 아이템을 보상으로 얻을 수 있다.");
-                Console.WriteLine();
-                Console.WriteLine("1. 골드를 얻는다");
-                Console.WriteLine("2. 아이템을 얻는다");
-                Console.WriteLine();
-                Console.WriteLine("0. 무시한다");
-                Console.WriteLine();
-                Console.WriteLine("무엇을 하시겠습니까?");
-                Console.WriteLine(">> ");
+            Console.WriteLine("전리품으로써 골드 혹은 아이템을 보상으로 얻을 수 있다.");
+            Console.WriteLine();
+            Console.WriteLine("1. 골드를 얻는다");
+            Console.WriteLine("2. 아이템을 얻는다");
+            Console.WriteLine();
+            Console.WriteLine("0. 무시한다");
+            Console.WriteLine();
+            Console.WriteLine("무엇을 하시겠습니까?");
+            Console.WriteLine(">> ");
 
-                string input;
-                input = Console.ReadLine();
+            string input;
+            input = Console.ReadLine();
 
-                if (input == "1")
+            if (input == "1")
+            {
+                player.Gold += 100;
+                Console.WriteLine("당신은 100 G를 얻었다.");
+            }
+
+            else if (input == "2")
+            {
+                //List<Item> itemList = new List<Item>();
+
+                Random random = new Random();
+
+                int index1 = random.Next(itemList.Count);
+                int index2 = random.Next(itemList.Count);
+
+                //index1을 먼저 검사 갖고있는지 아닌지
+                //index2를 검사할 때 갖고있나아닌가 index1이랑 동시에 검사
+                while (itemList[index1].IsOwned)
                 {
-                    player.Gold += 100;
-                    Console.WriteLine("당신은 100 G를 얻었다.");
+                    index1 = random.Next(itemList.Count);
                 }
 
-                else if (input == "2")
+
+                while (index2 == index1 && itemList[index2].IsOwned)
                 {
-                    //List<Item> itemList = new List<Item>();
-
-                    Random random = new Random();
-
-                    int index1 = random.Next(itemList.Count);
-                    int index2 = random.Next(itemList.Count);
-
-                    while (index2 == index1)
-                    {
-                        index2 = random.Next(itemList.Count);
-                    }
-
-                    Console.WriteLine($"{itemList[index1]}과 {itemList[index2]}중 무엇을 얻을까?");
-
-                    string input02;
-                    input02 = Console.ReadLine();
-
-                    if (input02 == "1")
-                    {
-                        
-                        player.Inventory.Inven.Add(itemList[index1]);
-
-                        Console.WriteLine($"{itemList[index1]}(을)를 획득했다!");
-                    }
-
-                    else
-                    {
-                        player.Inventory.Inven.Add(itemList[index1]);
-
-                        Console.WriteLine($"{itemList[index2]}(을)를 획득했다!");
-                    }
+                    index2 = random.Next(itemList.Count);
                 }
+
+                Console.WriteLine($"{itemList[index1].ItemName}과 {itemList[index2].ItemName}중 무엇을 얻을까?");
+
+
+                string input02;
+                input02 = Console.ReadLine();
+
+                if (input02 == "1")
+                {
+                    player.Inventory.Inven.Add(itemList[index1]);
+
+                    Console.WriteLine($"{itemList[index1]}(을)를 획득했다!");
+                }
+
+                else
+                {
+                    player.Inventory.Inven.Add(itemList[index1]);
+
+                    Console.WriteLine($"{itemList[index2]}(을)를 획득했다!");
+                }
+            }
 
             battleSystem.Battle();
 
@@ -156,7 +164,7 @@ namespace ConsoleRPG24
             GameClear();
         }
 
-        
+
         public void ShopEncounter()
         {
             Console.WriteLine("당신은 다음으로 나아가던 중, 던전 안에 숨겨져있던 비밀 상점을 발견했다.");
@@ -183,7 +191,7 @@ namespace ConsoleRPG24
                 DungeonStart();
             }
         }
-        
+
 
 
         public void GameClear()
